bcff3eda69f06b6e651b263639338b82c02c05ad
kateFrontend <ecdanilova@gmail.com>
2022-11-16 20:39:58 -0500

    add main info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { data } from "./data";
 
 function App() {
+  const [hotels, setHotels] = useState(data);
+
   return (
     <div>
-      
+      <div className="container">
+        <h1>NYC TOP {hotels.length} HOTELS</h1>
+      </div>
+      {hotels.map((hotel) => {
+        const { id, hotelName, description, image, source } = hotel;
+        return (
+          <div key={id}>
+            <div className="container">
+              <h2>
+                {id} - {hotelName}
+              </h2>
+            </div>
+            <div className="container">
+              <p>{description}</p>
+            </div>
+            <div className="container">
+              <button className="btn" onClick={() => removeHotel(id)}>Remove</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
